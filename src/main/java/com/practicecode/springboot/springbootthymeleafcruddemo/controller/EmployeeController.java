531a2c0aac28f46ef3c0b70f38a67b6e26de7c9d
ravidugm <ravindumiyuranga@gmail.com>
2021-04-02 21:15:25 +0530

    Update records in the database

--- src/main/java/com/practicecode/springboot/springbootthymeleafcruddemo/controller/EmployeeController.java
+++ src/main/java/com/practicecode/springboot/springbootthymeleafcruddemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.practicecode.springboot.springbootthymeleafcruddemo.service.EmployeeS
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -44,6 +41,19 @@ public class EmployeeController {
 
         theModel.addAttribute("employee", theEmployee);
 
+        return "employees/employee-form";
+    }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel) {
+
+        // get the employee form the service
+        Employee theEmployee = employeeService.findById(theId);
+
+        // set employee as a model attribute to pre-populated the form
+        theModel.addAttribute("employee", theEmployee);
+
+        // send over to form
         return "employees/employee-form";
     }
 
